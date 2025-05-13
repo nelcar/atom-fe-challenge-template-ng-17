ae0b9b743b944e0fa2a6582d5722924759b1c105
nelcar <nelsonjc.01@gmail.com>
2025-05-12 23:52:07 -0600

    feat: update Angular dependencies and enhance login component with form functionality and styling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from "@angular/core";
-import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
-import { provideRouter } from "@angular/router";
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
-import { routes } from "./app.routes";
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync()
+  ],
 };
--- src/app/modules/login/pages/login/login.component.ts
+++ src/app/modules/login/pages/login/login.component.ts
@@ -3,11 +3,20 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AuthService } from '../../../../core/services/auth.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
   selector: 'app-login',
-  imports: [FormsModule, HttpClientModule],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
