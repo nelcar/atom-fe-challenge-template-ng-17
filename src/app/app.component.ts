a4f981b36c6c9b43a60e0d6a7970b467b6f56385
nelcar <nelsonjc.01@gmail.com>
2025-05-13 00:40:44 -0600

    feat: implement loading service and interceptor with progress bar in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { LoadingService } from "./core/services/loading.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: "app-root",
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CommonModule, MatProgressBarModule],
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.scss"
 })
 export class AppComponent {
-    title = "atom-challenge-fe-template";
+    loading$ = this.loadingService.loading$;
+
+    constructor(private loadingService: LoadingService) {}
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([loadingInterceptor])),
     provideAnimationsAsync()
   ],
 };
